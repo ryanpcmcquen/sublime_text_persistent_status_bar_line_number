3e8c32e5d0e9529220dbbe0cc0d38a0193256443
Ryan McQuen <rmcquen@funko.com>
2019-08-20 14:13:48 -0700

    Add README and change name.
    Signed-off-by: Ryan McQuen <rmcquen@funko.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# Persistent Status Bar Line Number
+
+Adds a persistent status bar line number, so even when searching in Sublime you can know what line you are on, even if you roll with `line_numbers` disabled in the gutter.
+
+
--- persistent_status_bar_line_number.py
+++ persistent_status_bar_line_number.py
@@ -2,11 +2,8 @@ import sublime
 import sublime_plugin
 
 
-class PersistentStatusBarLineNumbers(sublime_plugin.EventListener):
+class PersistentStatusBarLineNumber(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
-        # def run(self, edit):
-        # In the console this is given as:
-        # view.rowcol(view.sel()[0].begin())[0] + 1
         current_line_number = view.rowcol(
             view.sel()[0].begin()
         )[0] + 1
