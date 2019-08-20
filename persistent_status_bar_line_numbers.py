adbe2e658228d5227290696d91485c0478e040a2
Ryan McQuen <rmcquen@funko.com>
2019-08-20 13:50:57 -0700

    Improve formatting.
    Signed-off-by: Ryan McQuen <rmcquen@funko.com>

--- persistent_status_bar_line_numbers.py
+++ persistent_status_bar_line_numbers.py
@@ -6,12 +6,7 @@ class PersistentStatusBarLineNumbers(sublime_plugin.TextCommand):
     def run(self, edit):
         # In the console this is given as:
         # view.rowcol(view.sel()[0].begin())[0] + 1
-        current_line_number = self
-        .view
-        .rowcol(
-            self
-            .view
-            .sel()[0]
-            .begin()
+        current_line_number = self.view.rowcol(
+            self.view.sel()[0].begin()
         )[0] + 1
         set_status('persistent_line_number', current_line_number)
