9af9b8c63641dbc0e5daabc5920a11abea47790d
Ryan McQuen <rmcquen@funko.com>
2019-08-20 13:40:20 -0700

    Initial commit.
    Signed-off-by: Ryan McQuen <rmcquen@funko.com>

---
+++ persistent_status_bar_line_numbers.py
@@ -0,0 +1,6 @@
+
+
+# In the console this is given as:
+# view.rowcol(view.sel()[0].begin())[0] + 1
+current_line_number = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
+set_status('persistent_line_number', current_line_number)
