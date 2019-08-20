c46a16daf7ddd0bb15b875f714fcbeab243794b6
Ryan McQuen <rmcquen@funko.com>
2019-08-20 14:05:28 -0700

    Fire on event.
    Signed-off-by: Ryan McQuen <rmcquen@funko.com>

--- persistent_status_bar_line_numbers.py
+++ persistent_status_bar_line_numbers.py
@@ -2,12 +2,12 @@ import sublime
 import sublime_plugin
 
 
-class PersistentStatusBarLineNumbers(sublime_plugin.TextCommand):
-    def run(self, edit):
+class PersistentStatusBarLineNumbers(sublime_plugin.EventListener):
+    def on_selection_modified(self, view):
+        # def run(self, edit):
         # In the console this is given as:
         # view.rowcol(view.sel()[0].begin())[0] + 1
-        current_line_number = self.view.rowcol(
-            self.view.sel()[0].begin()
+        current_line_number = view.rowcol(
+            view.sel()[0].begin()
         )[0] + 1
-        self.view.set_status('persistent_line_number', current_line_number)
-
+        view.set_status('persistent_line_number', current_line_number)
