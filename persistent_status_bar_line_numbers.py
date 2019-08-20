edff4f65b949897f597e20b46c46ec1aee89fefa
Ryan McQuen <rmcquen@funko.com>
2019-08-20 13:49:43 -0700

    Add class/boilerplate.
    Signed-off-by: Ryan McQuen <rmcquen@funko.com>

--- persistent_status_bar_line_numbers.py
+++ persistent_status_bar_line_numbers.py
@@ -1,7 +1,17 @@
 import sublime
 import sublime_plugin
 
-# In the console this is given as:
-# view.rowcol(view.sel()[0].begin())[0] + 1
-current_line_number = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
-set_status('persistent_line_number', current_line_number)
+
+class PersistentStatusBarLineNumbers(sublime_plugin.TextCommand):
+    def run(self, edit):
+        # In the console this is given as:
+        # view.rowcol(view.sel()[0].begin())[0] + 1
+        current_line_number = self
+        .view
+        .rowcol(
+            self
+            .view
+            .sel()[0]
+            .begin()
+        )[0] + 1
+        set_status('persistent_line_number', current_line_number)
