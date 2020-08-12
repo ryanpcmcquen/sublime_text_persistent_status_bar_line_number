be130ef360dcb6e432455a4b85fca9d26fc0d193
Ryan McQuen <rpcm@linux.com>
2020-08-12 11:59:34 -0700

    Ignore bad indices.

--- persistent_status_bar_line_number.py
+++ persistent_status_bar_line_number.py
@@ -4,7 +4,13 @@ import sublime_plugin
 
 class PersistentStatusBarLineNumber(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
-        current_line_number = view.rowcol(
-            view.sel()[0].begin()
-        )[0] + 1
-        view.set_status('persistent_line_number', str(current_line_number))
+        try:
+            current_line_number = view.rowcol(
+                view.sel()[0].begin()
+            )[0] + 1
+            view.set_status('persistent_line_number', str(current_line_number))
+        except IndexError:
+            # Sometimes we just get weird indices. There's no
+            # need to fill up the Sublime Console
+            # with those errors.
+            pass
